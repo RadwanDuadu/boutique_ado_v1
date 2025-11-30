236b0a400574d0a4e6b0ab4631276d9868258a96
RadwanDuadu <mradwan444@gmail.com>
2025-11-30 22:46:29 +0000

    Add products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -158,4 +159,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
